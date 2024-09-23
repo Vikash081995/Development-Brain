2453b7b679d42dbc69f76b25d7525b4af19e5bae
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-09-24 02:29:07 +0530

    eventemitter and bubble sort

---
+++ NODE-BRAIN/Event-emitter.js
@@ -0,0 +1,7 @@
+import { EventEmitter } from "node:events";
+const emitter = new EventEmitter();
+emitter.on("message", (message) => {
+  console.log(`Message received:${message}`);
+});
+
+emitter.emit("message", "Hello World!");
