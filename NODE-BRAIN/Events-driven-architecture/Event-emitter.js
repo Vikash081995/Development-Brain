fdf59c3080b992fd3cdeabc6cb163d56e8d39f46
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-09-29 02:45:42 +0530

    Nodejs-express setup

--- NODE-BRAIN/Events-driven-architecture/Event-emitter.js
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from "node:events";
-const emitter = new EventEmitter();
-emitter.on("message", (message) => {
-  console.log(`Message received:${message}`);
-});
-
-emitter.emit("message", "Hello World!");
