c03722082b46e87a00ffd4bc4fc22de86133f3a2
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-10-05 22:49:27 +0530

    python day1+ nodejs

---
+++ Python-brain/basic-demo/meaning.py
@@ -0,0 +1,7 @@
+meaning = 42
+print("")
+
+if meaning>42 :
+    print('Right on!')
+else: 
+    print('Not today')
---
+++ Python-brain/basic-demo/welcome.py
@@ -0,0 +1,11 @@
+line01="************************************"
+line02="************************************"
+line03="****WELCOME**************************"
+
+
+
+#starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
