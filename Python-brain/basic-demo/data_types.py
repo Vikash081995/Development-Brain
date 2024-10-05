fa92a2e15e910c6351e86003aec0a58f3ccdd60f
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-10-05 22:57:12 +0530

    data types in python

---
+++ Python-brain/basic-demo/data_types.py
@@ -0,0 +1,21 @@
+# String data type
+
+#literal assignment 
+first="Dave"
+last="Gray"
+
+print(type(first))
+print(type(first) ==str)
+print(isinstance(first,str))
+# <class 'str'>
+# True
+# True
+
+# Constructor function
+pizza = str("Pepperoni")
+print(type(pizza));
+print(type(pizza)==str);
+print(isinstance(pizza,str))
+# <class 'str'>
+# True
+# True
